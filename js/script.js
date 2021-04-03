8980f6ebb99f4c8e5f3438a72a91874efed10593
kristina2204 <kristina.kirova.00@bk.ru>
2021-04-03 11:34:49 +0300

    lesson 2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-let hello;
-hello='Привет';
-console.log(hello);
+let num;
+let score;
+const questions = [
+	{ question: 'question1', answer1: 'a', correctAnswer: 'b', answer3: 'c' },
+	{ question: 'question2', answer1: 'a', answer2: 'b', correctAnswer: 'c' },
+	{ question: 'question3', correctAnswer: 'a', answer2: 'b', answer3: 'c' },
+	{ question: 'question4', answer1: 'a', answer2: 'b', correctAnswer: 'c' },
